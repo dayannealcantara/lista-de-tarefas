6ab71416c48b88ec594026205a9c9b86170f977b
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-09 14:31:58 -0300

    feat:creating listing screen style

---
+++ src/App.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height:100vh;
+  background:#000;
+`;
+export const Wrapper = styled.div`
+  margin:auto;
+  max-width:980px;
+  padding:10px; 
+`;
+export const Header = styled.h1`
+  margin:0;
+  padding:0;
+  padding-bottom:20px;
+  color:#fff;
+  text-align:center;
+  border-bottom:1px solid #444
+`;
