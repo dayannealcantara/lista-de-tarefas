0b1f7e4c03c2b3fe16cd6683754a1133ad2fc1e3
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-10 13:25:06 -0300

    feat: style to-do list

---
+++ src/Components/ListAdd/ListAdd.styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const WrapperListAdd = styled.div`
+  display:flex;
+ border:1px solid #555 ;
+  padding:10px;
+  border-radius:15px;
+  margin: 20px 0;
+  align-items:center;
+  .image{
+    margin-right:10px ;
+  }
+
+  input{
+    border:0;
+    background:transparent;
+    outline:0;
+    color:#fff;
+    font-size:18px;
+    flex:1
+    
+  }
+  
+  
+  `
